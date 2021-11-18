5e33a1210eb87b980398e6848284599fdbcfbadb
Adam KWP Landberg <lanadam@student.chalmers.se>
2021-11-18 14:33:37 +0100

    Adams grejer

--- src/Saab95.java
+++ src/Saab95.java
@@ -6,8 +6,20 @@ import java.awt.*;
  */
 public class Saab95 extends Car{
 
+    /**
+     * Creates the function for turbo, where a true value sets the turbo on, and a false value sets the turbo to off.
+     * */
     private boolean turboOn;
-    
+
+    /**
+     * Defines the specific car type Saab 95 and
+     * sets the number of doors to 2
+     * sets the color to red
+     * sets the power of the engine to 125
+     * sets the turbo to off by calling a method from below
+     * sets the name of the model tp Saab95
+     * initializes the method stop engine which assures that the car does not move it is created
+     */
     public Saab95(){
         setNrDoors(2);
         setColor(Color.red);
@@ -18,22 +30,25 @@ public class Saab95 extends Car{
     }
 
     /**
-     * turns on the turbo
+     * turns the turbo on
      */
     public void setTurboOn(){
 	    turboOn = true;
     }
 
     /**
-     * turns off the turbo
+     * turns the turbo off
      */
     public void setTurboOff(){
 	    turboOn = false;
     }
 
+ 
     /**
-     * a method that calculates a factor for the calculation of a viechle's acceleration
-     * @return
+     * The speed factor returns the acceleration of the Saab95 Car calculated by inserting the turbo, which if it
+     * is off returns a factor of 1, and if it is on returns a factor of 1.3.
+     * inserting the engine power from above, multiplied by a constant factor, multiplied with the turbo factor
+     * from above.
      */
     public double speedFactor(){
         double turbo = 1;
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -2,13 +2,23 @@ package src;
 
 import java.awt.*;
 /**
- * klassen Volvo240 är en subklass til Car och innehåller värden och metoder för en
- * specifik biltyp.
+ * the Class Volvo240 is a sub class to Car and contains values and methods for a specific type of car.
  */
 public class Volvo240 extends Car{
 
+    /**
+     * Sets a fixed value of the trim factor that is used in the calculation of the speed of the car.
+     */
     private final static double trimFactor = 1.25;
-    
+
+    /**
+     * Defines the specific car type Volvo240 and
+     * sets the number of doors to 4
+     * sets the color to black
+     * sets the power of the engine to 100
+     * sets the name of the model tp Volvo240
+     * initializes the method stop engine which assures that the car does not move it is created
+     */
     public Volvo240(){
         setNrDoors(4);
         setColor(Color.black);
@@ -16,9 +26,10 @@ public class Volvo240 extends Car{
         setModelName("Volvo240");
         stopEngine();
     }
+
     /**
-     * a method that calculates a factor for the calculation of a veichle's acceleration
-     * @return
+     * The speed factor returns the acceleration of the Volvo240 Car calculated by inserting the engine power
+     * from above, multiplied by a constant factor, multiplied with the trim factor from above.
      */
     @Override
     double speedFactor(){
